693a58a220aa5be6819125f11ede66ab8f29a246
尘外孤标 <3334684495@qq.com>
2020-03-17 23:02:01 +0800

    test

--- app/src/main/java/com/example/listview/DetailActivity.java
+++ app/src/main/java/com/example/listview/DetailActivity.java
@@ -15,8 +15,10 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
         Intent intent = getIntent();
         String studentId = intent.getStringExtra("studentId");
+		
         ImageView imgDetail = findViewById(R.id.detail_img);
         imgDetail.setImageResource(R.drawable.ic_launcher_background);
+		
         TextView textDetail = findViewById(R.id.detail_text);
         textDetail.setText(studentId);
     }
